51fe2c95cb3174662a385501b6a5a530c053820c
VadNerov22 <vadoleg@mail.ru>
2023-06-18 21:26:05 +0300

    Выполненная ЛР 2 модуля 3.

--- Занятие 2. Атрибуты и методы/2. Лабораторные задания/Класс Book/main.py
+++ Занятие 2. Атрибуты и методы/2. Лабораторные задания/Класс Book/main.py
@@ -12,7 +12,17 @@ BOOKS_DATABASE = [
 ]
 
 
-# TODO написать класс Book
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id_ = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f"Book(id_={self.id_}, name={self.name!r}, pages={self.pages})"
 
 
 if __name__ == '__main__':
--- Занятие 2. Атрибуты и методы/2. Лабораторные задания/Класс Library/main.py
+++ Занятие 2. Атрибуты и методы/2. Лабораторные задания/Класс Library/main.py
@@ -12,10 +12,41 @@ BOOKS_DATABASE = [
 ]
 
 
-# TODO написать класс Book
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id_ = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f"Book(id_={self.id_}, name={self.name!r}, pages={self.pages})"
 
 
-# TODO написать класс Library
+class Library:
+    def __init__(self, books: list[Book] = None):
+        if books is None:
+            self.books = []
+        else:
+            self.books = books
+
+    def get_next_book_id(self):
+        if not self.books:
+            return 1
+        else:
+            return self.books.__getitem__(-1).id_ + 1
+
+    def __str__(self):
+        return f"{self.books}"
+
+    def get_index_by_book_id(self, index_: int):
+        for i, x in enumerate(self.books):
+            if self.books.__getitem__(i).id_ == index_:
+                return i
+        else:
+            raise ValueError(f"Книги с запрашиваемым id не существует")
 
 
 if __name__ == '__main__':
