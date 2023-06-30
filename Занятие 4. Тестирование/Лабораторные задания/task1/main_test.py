53b244a0793b9e408dcf9eb4426d152548e3f6d1
VadNerov22 <vadoleg@mail.ru>
2023-07-01 02:16:49 +0300

    Выполненная ЛР 4 Модуля 3

--- Занятие 4. Тестирование/Лабораторные задания/task1/main_test.py
+++ Занятие 4. Тестирование/Лабораторные задания/task1/main_test.py
@@ -1,10 +1,36 @@
 import unittest
-from main import *  # импортируем то, что будем тестировать
+from main import Cars
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def test_car_capacity_volume_f(self):
+        self.assertRaises(TypeError, Cars.car_capacity_volume_f, "212")
+
+    def test_car_occupied_volume_f(self):
+        self.assertRaises(TypeError, Cars.car_occupied_volume_f, "158")
+
+    def test_is_valid_car_type(self):
+        self.assertTrue(Cars.is_valid_car_type("седан"))
+        self.assertRaises(TypeError, Cars.is_valid_car_type, 15)
+        self.assertRaises(ValueError, Cars.is_valid_car_type, "джип")
+
+    def test_mile_h_to_km_h(self):
+        self.assertEqual(Cars.mile_h_to_km_h(90), 144)
+
+    def test_to_kbt(self):
+        self.assertEqual(Cars.to_kbt(150), 110)
+
+    def setUp(self):
+        self._car_capacity_volume_f = 58
+        self._car_occupied_volume_f = 20
+
+    def test_power_reserve(self):
+        self.assertEqual(Cars.power_reserve(self, 380), 200)
+
+    def test_is_valid_init(self):
+        self.assertRaises(TypeError, Cars.is_valid_init, 120.15)
+        self.assertRaises(ValueError, Cars.is_valid_init, -15)
 
 
 if __name__ == '__main__':
